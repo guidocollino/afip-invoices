37259a8343bd933b53f752aa9263a1ed3c7c527c
Guido Collino <collinoguido@gmail.com>
2026-03-10 20:41:51 -0300

    Revert bounding_box removal in display_items_and_totals
    The bounding_box is needed to position items below the header area.
    Without it, auto-paginated table content overlaps with the header
    on subsequent pages.

--- app/pdfs/invoice_pdf.rb
+++ app/pdfs/invoice_pdf.rb
@@ -219,13 +219,13 @@ class InvoicePdf < ToPdf
   def display_items_and_totals
     stretchy_cursor = invoice_is_fce? ? 460 - 35 : 480 - 35
 
-    move_cursor_to stretchy_cursor
+    bounding_box([0, stretchy_cursor], width: 540, height: 330) do
+      display_items
 
-    display_items
+      display_note if @invoice.note.present?
 
-    display_note if @invoice.note.present?
-
-    display_totals
+      display_totals
+    end
   end
 
   def display_items
